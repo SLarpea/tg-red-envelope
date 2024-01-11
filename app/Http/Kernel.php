444f009ae91a748452d1c97a4f7f45d4d07d1f75
DESKTOP-0LCEUDU\Franz <fmhorfilla.wbridges@gmail.com>
2024-01-11 12:54:22 +0800

    route permission

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,6 +68,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'setLocale' => \App\Http\Middleware\SetLocale::class
+        'setLocale' => \App\Http\Middleware\SetLocale::class,
+        'permission' => \App\Http\Middleware\CheckPermission::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -25,31 +25,31 @@ Route::middleware([
     'verified',
     'setLocale'
 ])->group(function () {
-    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/recharge', [App\Http\Controllers\RechargeRecordController::class, 'index'])->name('recharge');
-    Route::get('/rewards', [App\Http\Controllers\RewardRecordController::class, 'index'])->name('rewards');
-    Route::get('/reports', [App\Http\Controllers\ReportController::class, 'index'])->name('reports');
-    Route::get('/commissions', [App\Http\Controllers\CommissionRecordController::class, 'index'])->name('commissions.index');
-    Route::get('/red-envelopes', [App\Http\Controllers\RedEnvelopeManagementController::class, 'index'])->name('red-envelopes.index');
-    Route::get('/withdraw', [App\Http\Controllers\WithdrawRecordController::class, 'index'])->name('withdraw');
-    Route::get('/operation-log', [App\Http\Controllers\OperationLogController::class, 'index'])->name('operation-log');
-    Route::get('/personal-report', [App\Http\Controllers\PersonalReportController::class, 'index'])->name('personal-report');
-    Route::post('/personal-report', [App\Http\Controllers\PersonalReportController::class, 'index'])->name('post.personal-report');
-    Route::get('/funding-details', [App\Http\Controllers\FundingDetailController::class, 'index'])->name('funding-details');
-    Route::get('/lucky-history', [App\Http\Controllers\LuckyHistoryController::class, 'index'])->name('lucky-history');
-    Route::get('/invitation-records', [App\Http\Controllers\InvitationRecordController::class, 'index'])->name('invitation-records');
-    Route::get('/share-records', [App\Http\Controllers\ShareRecordController::class, 'index'])->name('share-records');
-    Route::get('/winning-records', [App\Http\Controllers\WinningRecordController::class, 'index'])->name('winning-records');
-    Route::resource('/administrator', App\Http\Controllers\AdministratorController::class);
-    Route::resource('/roles', App\Http\Controllers\RoleController::class);
-    Route::resource('/permissions', App\Http\Controllers\PermissionController::class);
-    Route::resource('/groups', App\Http\Controllers\GroupManagementController::class);
-    Route::resource('/configs', App\Http\Controllers\ConfigController::class);
-    Route::resource('/tg-users', App\Http\Controllers\UserManagementController::class);
-    Route::put('/tg-users.top-up/{id}', [App\Http\Controllers\UserManagementController::class, 'top_up'])->name('tg-users.top-up');
-    Route::put('/tg-users.withdraw/{id}', [App\Http\Controllers\UserManagementController::class, 'withdraw'])->name('tg-users.withdraw');
-    Route::resource('/menus', App\Http\Controllers\MenuController::class);
-    Route::put('/menus.sort', [App\Http\Controllers\MenuController::class, 'sort'])->name('menus.sort');
+    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard')->middleware('permission:View Dashboard');
+    Route::get('/recharge', [App\Http\Controllers\RechargeRecordController::class, 'index'])->name('recharge')->middleware('permission:View Recharge Record');
+    Route::get('/rewards', [App\Http\Controllers\RewardRecordController::class, 'index'])->name('rewards')->middleware('permission:View Recharge Record');
+    Route::get('/reports', [App\Http\Controllers\ReportController::class, 'index'])->name('reports')->middleware('permission:View Reports');
+    Route::get('/commissions', [App\Http\Controllers\CommissionRecordController::class, 'index'])->name('commissions.index')->middleware('permission:View Platform Commission Record');
+    Route::get('/red-envelopes', [App\Http\Controllers\RedEnvelopeManagementController::class, 'index'])->name('red-envelopes.index')->middleware('permission:View Red Envelope Management');
+    Route::get('/withdraw', [App\Http\Controllers\WithdrawRecordController::class, 'index'])->name('withdraw')->middleware('permission:View Withdrawal Record');
+    Route::get('/operation-log', [App\Http\Controllers\OperationLogController::class, 'index'])->name('operation-log')->middleware('permission:View Operation Log');
+    Route::get('/personal-report', [App\Http\Controllers\PersonalReportController::class, 'index'])->name('personal-report')->middleware('permission:Can View Personal Report in User Management');
+    Route::post('/personal-report', [App\Http\Controllers\PersonalReportController::class, 'index'])->name('post.personal-report')->middleware('permission:Can View Personal Report in User Management');
+    Route::get('/funding-details', [App\Http\Controllers\FundingDetailController::class, 'index'])->name('funding-details')->middleware('permission:Can View Funding Details in User Management');
+    Route::get('/lucky-history', [App\Http\Controllers\LuckyHistoryController::class, 'index'])->name('lucky-history')->middleware('permission:Can View Lucky History in User Management');
+    Route::get('/invitation-records', [App\Http\Controllers\InvitationRecordController::class, 'index'])->name('invitation-records')->middleware('permission:View Invitation Record in User Management');
+    Route::get('/share-records', [App\Http\Controllers\ShareRecordController::class, 'index'])->name('share-records')->middleware('permission:Can View Share Record in User Management');
+    Route::get('/winning-records', [App\Http\Controllers\WinningRecordController::class, 'index'])->name('winning-records')->middleware('permission:Can View Winning Record in User Management');
+    Route::resource('/administrator', App\Http\Controllers\AdministratorController::class)->middleware('permission:View Administrator');
+    Route::resource('/roles', App\Http\Controllers\RoleController::class)->middleware('permission:View Role');
+    Route::resource('/permissions', App\Http\Controllers\PermissionController::class)->middleware('permission:View Permissions');
+    Route::resource('/groups', App\Http\Controllers\GroupManagementController::class)->middleware('permission:View Group Management');
+    Route::resource('/configs', App\Http\Controllers\ConfigController::class)->middleware('permission:Can Configure in Group Management');
+    Route::resource('/tg-users', App\Http\Controllers\UserManagementController::class)->middleware('permission:View User Management');
+    Route::put('/tg-users.top-up/{id}', [App\Http\Controllers\UserManagementController::class, 'top_up'])->name('tg-users.top-up')->middleware('permission:View User Management');
+    Route::put('/tg-users.withdraw/{id}', [App\Http\Controllers\UserManagementController::class, 'withdraw'])->name('tg-users.withdraw')->middleware('permission:View User Management');
+    Route::resource('/menus', App\Http\Controllers\MenuController::class)->middleware('permission:View Menu');
+    Route::put('/menus.sort', [App\Http\Controllers\MenuController::class, 'sort'])->name('menus.sort')->middleware('permission:View Menu');
 
     Route::post('/set-locale', [App\Http\Controllers\UserManagementController::class, 'setLocale'])->name('post.setlocale');
 
