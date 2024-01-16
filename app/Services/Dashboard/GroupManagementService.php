548f2a82a064d017ebbe93841410e29806e5c0e0
phpjhonar <jhonarignacio.wbridges@gmail.com>
2024-01-16 11:23:03 +0800

    [update & fix] group management update along with configs

--- app/Services/Dashboard/GroupManagementService.php
+++ app/Services/Dashboard/GroupManagementService.php
@@ -4,6 +4,7 @@ namespace App\Services\Dashboard;
 
 use App\Models\Config;
 use App\Models\GroupManagement;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Session;
@@ -23,43 +24,12 @@ class GroupManagementService
 
     public function storeData($request)
     {
-        $request->validated();
-        GroupManagement::create([
-            'group_id' => $request->group_id,
-            'name' => $request->name,
-            'remark' => $request->remark,
-            'status' => $request->status,
-            'service_url' => $request->service_url,
-            'recharge_url' => $request->recharge_url,
-            'channel_url' => $request->channel_url,
-            'photo_id' => $request->photo_id,
-            'admin_id' => $request->admin_id,
-        ]);
-
-        $tgbotConfig = config('tgbot');
-        foreach ($tgbotConfig as $key => $val) {
-            if (Config::query()->where('name', $key)->where('group_id', $request->group_id)->count() == 0) {
-                $insert = [
-                    'name' => $key,
-                    'value' => $val,
-                    'group_id' => $request->group_id,
-                    'admin_id' => 1,
-                    'remark' => trans('admin.tgbot.' . $key),
-                ];
-                Config::create($insert);
-            }
-        }
-
-    }
-
-    public function updateData($request)
-    {
-        if($request->update_type == 'all'){
+        DB::beginTransaction();
+        try {
             $request->validated();
-
-            GroupManagement::find($request->input('id'))->update([
-                'name' => $request->name,
+            GroupManagement::create([
                 'group_id' => $request->group_id,
+                'name' => $request->name,
                 'remark' => $request->remark,
                 'status' => $request->status,
                 'service_url' => $request->service_url,
@@ -68,10 +38,68 @@ class GroupManagementService
                 'photo_id' => $request->photo_id,
                 'admin_id' => $request->admin_id,
             ]);
-        }else{
-            GroupManagement::find($request->input('id'))->update([
-                'status' => ($request->status == 1) ? 0 : 1,
-            ]);
+
+            $tgbotConfig = config('tgbot');
+            foreach ($tgbotConfig as $key => $val) {
+                if (Config::query()->where('name', $key)->where('group_id', $request->group_id)->count() == 0) {
+                    $insert = [
+                        'name' => $key,
+                        'value' => $val,
+                        'group_id' => $request->group_id,
+                        'admin_id' => 1,
+                        'remark' => trans('admin.tgbot.' . $key),
+                    ];
+                    Config::create($insert);
+                }
+            }
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+        }
+    }
+
+    public function updateData($request)
+    {
+        DB::beginTransaction();
+        try {
+            if ($request->update_type == 'all') {
+                $request->validated();
+
+                $groupmanagement = GroupManagement::find($request->input('id'));
+
+                $groupIdToUpdate = $groupmanagement->group_id;
+
+                if ($groupmanagement) {
+                    $gupdate = $groupmanagement->update([
+                        'name' => $request->name,
+                        'group_id' => $request->group_id,
+                        'remark' => $request->remark,
+                        'status' => $request->status,
+                        'service_url' => $request->service_url,
+                        'recharge_url' => $request->recharge_url,
+                        'channel_url' => $request->channel_url,
+                        'photo_id' => $request->photo_id,
+                        'admin_id' => $request->admin_id,
+                    ]);
+
+                    if ($gupdate) {
+                        Config::where('group_id', $groupIdToUpdate)->update([
+                            'group_id' => $request->group_id
+                        ]);
+                    }
+                } else {
+                    // Handle not finding GroupManagement with the specified ID
+                }
+            } else {
+                GroupManagement::find($request->input('id'))->update([
+                    'status' => ($request->status == 1) ? 0 : 1,
+                ]);
+            }
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
         }
     }
 
